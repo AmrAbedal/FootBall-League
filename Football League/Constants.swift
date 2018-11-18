83264abf4bae829b9821579bf94b904a97f67ef6
Arabia-IT <alabeeb@arabia-it.com>
2018-11-18 10:38:34 +0200

    project initial setup

---
+++ Football League/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Football League
+//
+//  Created by Arabia-IT on 11/18/18.
+//  Copyright © 2018 Arabia-IT. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    
+    
+}
---
+++ Football League/NetworkManager.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManager.swift
+//  Football League
+//
+//  Created by Arabia-IT on 11/18/18.
+//  Copyright © 2018 Arabia-IT. All rights reserved.
+//
+
+import Foundation
+class NetworkManager {
+    
+    
+    
+    
+    
+    
+}
