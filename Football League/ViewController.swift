2d0a73aac2112cc8ee7b8b34d25f64bc9d331f61
Arabia-IT <alabeeb@arabia-it.com>
2018-11-18 10:15:56 +0200

    Initial Commit

---
+++ Football League/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Football League
+//
+//  Created by Arabia-IT on 11/18/18.
+//  Copyright © 2018 Arabia-IT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
