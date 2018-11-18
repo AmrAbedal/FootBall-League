d0e6aa4f4173ea3d8eaf2d85bb8e02f8d69b6e09
Arabia-IT <alabeeb@arabia-it.com>
2018-11-18 18:01:18 +0200

    adding uiview Extension to enable change border color , width , and corner radius

--- Football League/Scenes/Leugue Scene/LeagueCell.swift
+++ Football League/Scenes/Leugue Scene/LeagueCell.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class LeagueCell: UITableViewCell {
-  static let identifier = "leagueCell"
     
+    static let identifier = "leagueCell"
+    @IBOutlet weak var matchCountLabel: UILabel!
+    @IBOutlet weak var teamsCountLabel: UILabel!
     @IBOutlet weak var containerView: UIView!
-    
     @IBOutlet weak var leagueNameLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setupViews()
     }
-
     
-
+    private func setupViews() {
+        let selectedBackgroundView = UIView.init()
+        selectedBackgroundView.backgroundColor = UIColor.lightGray
+        self.selectedBackgroundView = selectedBackgroundView
+    }
 }
---
+++ Football League/UIKItExtension.swift
@@ -0,0 +1,69 @@
+//
+//  UIKItExtension.swift
+//  Football League
+//
+//  Created by Arabia-IT on 11/18/18.
+//  Copyright © 2018 Arabia-IT. All rights reserved.
+//
+
+import Foundation
+import UIKit
+extension UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get
+        {
+            return layer.cornerRadius
+        }
+        set (newRadius)
+        {
+            if newRadius >= 0
+            {
+                layer.cornerRadius = newRadius
+            }
+            else
+            {
+                layer.cornerRadius = 0
+            }
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get
+        {
+            return layer.borderWidth
+        }
+        set (newWidth)
+        {
+            if newWidth >= 0
+            {
+                layer.borderWidth = newWidth
+            }
+            else
+            {
+                layer.borderWidth = 0
+            }
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get
+        {
+            if let _ = layer.borderColor
+            {
+                return UIColor(cgColor: layer.borderColor!)
+            }
+            else
+            {
+                return UIColor()
+            }
+            
+        }
+        set (newColor)
+        {
+            layer.borderColor = newColor?.cgColor
+        }
+    }
+}
+
+
