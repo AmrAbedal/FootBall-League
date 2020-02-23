f5b0b79b4ecaebb7f88c6dd9a57180de5eb3c9d5
Amr Abdel-Wahab <amr@orcas.io>
2020-02-23 22:20:47 +0200

    setup repository for fetching first from api then check database

--- Football League/Scenes/Leugue Scene/fetchLeaguesUseCase.swift
+++ Football League/Scenes/Leugue Scene/fetchLeaguesUseCase.swift
@@ -8,18 +8,31 @@
 
 import Foundation
 import RxSwift
+import RealmSwift
 
 func fetchLeagues(dataSource: LeaguesDataSource, localDataSource: LeaguesDataSource) -> Single<LeaguesScreenData> {
+    func addObjectsToRealm(objects: List<League>) {
+        for object in objects {
+            DataBaseManager.shared.addOrUpdateObject(object: object)
+        }
+    }
+    
     return dataSource.getLeagues()
         .map({
+            addObjectsToRealm(objects: $0.leagues)
             return $0.screenData
         })
-        .catchError({ error in
+        .catchError({ _ in
             localDataSource.getLeagues()
-                .map({return $0.screenData})
-                .catchError({
-                    error in
-                    return .just(.failure(error: "No Iternet"))
+                .map({
+                    if !$0.leagues.isEmpty {
+                        return $0.screenData
+                    } else {
+                        return .failure(error: "No Iternet OR Cached Data ")
+                    }
+                })
+                .catchError({ _ in
+                    return .just(.failure(error: "No Iternet OR Cached Data"))
                 })
             }
     )
